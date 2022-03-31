e4873fd5bdc04aa086c0a409a585910d676f6bef
Abinesh241 <97597694+Abinesh241@users.noreply.github.com>
2022-03-31 18:49:23 +0530

    Add files via upload

---
+++ LoginRequest.java
@@ -0,0 +1,15 @@
+package com.example.carpooling.io;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class LoginRequest {
+    private String username;
+    private String password;
+}
